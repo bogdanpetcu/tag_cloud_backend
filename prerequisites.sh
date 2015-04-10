a71168ece851f429cfd3e7d95aa3a4e5f971f6f3
bogdan petcu <bogdanconstantinpetcu@gmail.com>
2015-04-10 22:10:14 +0300

    Added readme

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install python
+sudo apt-get install python-pip
 sudo pip install tweepy
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
@@ -9,4 +11,6 @@ cd ..
 sudo redis-server &
 sudo git clone https://github.com/andymccurdy/redis-py.git
 cd redis-py
-sudo python setup.py install
+sudo python setup.py install 
+cd ..
+rm redis-stable.tar.gz
